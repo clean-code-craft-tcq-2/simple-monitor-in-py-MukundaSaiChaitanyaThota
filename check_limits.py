25274ed2c500d038af683b0a3683e045042a4e94
MukundaSaiChaitanyaThota <91048236+MukundaSaiChaitanyaThota@users.noreply.github.com>
2022-02-08 22:47:31 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,27 +1,43 @@
+def PrintLowerthanRange(parameter,value):
+  print(parameter ,'with the reading',value,'is below the range!')
+  
+def PrintHigherthanRange(parameter,value):
+  print(parameter ,'with the reading',value,'is above the range!')
+  
 def isTemperatureInRange(temperature):
-  if (temperature < 0 or temperature > 45):
-    print('Temperature is out of range!')
+  if temperature < 0 :
+    PrintLowerthanRange("Temperature",temperature)
+    return False
+  elif temperature > 45 :
+    PrintHigherthanRange("Temperature",temperature)
     return False
   else:
     return True
   
 def issocInRange(soc):
-  if (soc < 20 or soc > 80):
-    print('soc is out of range!')
+  if soc < 20 :
+    PrintLowerthanRange("soc",soc)
+    return False
+  elif soc > 80 :
+    PrintHigherthanRange("soc",soc)
     return False
   else:
     return True
   
 def ischarge_rateInRange(charge_rate):
   if charge_rate > 0.8:
-    print('charge_rate is out of range!')
+    PrintHigherthanRange("charge_rate",charge_rate)
     return False
   else:
     return True
   
 
 def battery_is_ok(temperature, soc, charge_rate):
-  return isTemperatureInRange(temperature) and issocInRange(soc) and ischarge_rateInRange(charge_rate)
+  Checktemperature=isTemperatureInRange(temperature)
+  Checksoc=issocInRange(soc)
+  Checkcharge_rate=ischarge_rateInRange(charge_rate)
+    
+  return Checktemperature and Checksoc and Checkcharge_rate
 
 
 if __name__ == '__main__':
