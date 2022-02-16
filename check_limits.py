4dce135587b11883ff16da3ab13aa54ace9af4d6
MukundaSaiChaitanyaThota <91048236+MukundaSaiChaitanyaThota@users.noreply.github.com>
2022-02-16 09:40:50 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,42 +1,48 @@
-def PrintLowerthanRange(parameter,value):
-  print(parameter ,'with the reading',value,'is below the range!')
+def PrintoutofRange(parameter,value):
+  print(parameter ,'with the reading',value,'is out of the range!')
   
-def PrintHigherthanRange(parameter,value):
-  print(parameter ,'with the reading',value,'is above the range!')
   
 def isTemperatureInRange(temperature):
   if temperature < 0 :
-    PrintLowerthanRange("Temperature",temperature)
+    PrintoutofRange("Temperature",temperature)
     return False
   elif temperature > 45 :
-    PrintHigherthanRange("Temperature",temperature)
+    PrintoutofRange("Temperature",temperature)
     return False
   else:
     return True
   
 def issocInRange(soc):
   if soc < 20 :
-    PrintLowerthanRange("soc",soc)
+    PrintoutofRange("soc",soc)
     return False
   elif soc > 80 :
-    PrintHigherthanRange("soc",soc)
+    PrintoutofRange("soc",soc)
     return False
   else:
     return True
   
 def ischarge_rateInRange(charge_rate):
   if charge_rate > 0.8:
-    PrintHigherthanRange("charge_rate",charge_rate)
+    PrintoutofRange("charge_rate",charge_rate)
     return False
   else:
     return True
-  
+ 
+def convertTocelsius(temperature):
+  if('f' in temperature.lower() ):
+    return((int(temperature.lower().split('f')[0])-32.0)*(5.0/9.0))
+  elif('k' in temperature.lower()):
+    return(int(temperature.lower().split('k')[0])-273.0)
+  elif('c' in temperature.lower()):
+    return(int(temperature.lower().split('c')[0]))
 
 def battery_is_ok(temperature, soc, charge_rate):
   Checktemperature=isTemperatureInRange(temperature)
   Checksoc=issocInRange(soc)
   Checkcharge_rate=ischarge_rateInRange(charge_rate)
-    
+  
+  
   return Checktemperature and Checksoc and Checkcharge_rate
 
 
